f8252a998a2814a85d00c0b9dabefc8843188e05
maxandbe1 <maxandbe1@proton.me>
2026-05-27 22:38:28 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+console.log("%c🐝 Bee SIM Portal Runtime Loaded", "font-size: 16px; color: goldenrod;");
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
